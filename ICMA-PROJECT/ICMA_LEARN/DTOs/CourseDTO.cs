6e188b432b6a2e90b16b00a7aca8223e6d87502c
CodeWithMastu <masturah.moshood@gmail.com>
2024-10-28 04:14:50 +0100

    Add files via upload

---
+++ ICMA-PROJECT/ICMA_LEARN/DTOs/CourseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ICMA_LEARN.DTOs
+{
+    public class CourseDTO
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? category { get; set; }
+    }
+}
